5818bcc707543ab902ae6ff57024e8bbfa868621
Alexander Montero <a.m.v22305@gmail.com>
2023-11-15 19:09:32 -0600

    Api y rest

--- restserver/src/API.java
+++ restserver/src/API.java
@@ -9,13 +9,23 @@ public class API {
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(9999), 0);
         server.createContext("/", exchange -> {
-
+            /*
+            * Implementar metodos de get
+            * tiene que devolver un json
+            * */
             if ("GET".equals(exchange.getRequestMethod())) {
                 String responseText = "Usando GET\n";
                 exchange.sendResponseHeaders(200, responseText.getBytes().length);
                 OutputStream output = exchange.getResponseBody();
                 output.write(responseText.getBytes());
                 output.flush();
+
+                /*
+                * Implementat metodos post
+                * no devuelve nada
+                * recibe un json
+                * */
+
             } else if (("POST".equals(exchange.getRequestMethod()))) {
                 System.out.println("Hello World");
                 Scanner s = new Scanner(exchange.getRequestBody()).useDelimiter("\\A");
